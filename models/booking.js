53561d708da253155fb5b0c3dba7ea9cb8825cae
Jonathan hannerfeldt <hannerfeldt@hotmail.se>
2020-08-24 14:16:59 +0200

    first push

---
+++ models/booking.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+const BookingSchema = new Schema({
+
+    amount: Number,
+    date: Date,
+    time: String,
+    guestId: String,
+    id: Number
+
+});
+
+const Booking = mongoose.model("Booking", BookingSchema);
+
+module.exports = Booking;
