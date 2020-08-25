5eb88e5bd9277d3390a3ae2b251d738c49e9fa93
unknown <henriksson.charlie@gmail.com>
2020-08-25 12:43:53 +0200

    added links and separate urls for post and get

--- router/main.js
+++ router/main.js
@@ -4,24 +4,37 @@ const router = express.Router();
 const BookingModel = require("../models/booking");
 const GuestModel = require("../models/guest");
 
-router.get("/", async (req, res) => {
+router.get("/",(req, res) => {
 
-  const guest = await GuestModel.find()
-  const booking = await BookingModel.find()
-
-  res.send(guest + booking);
+ res.render("index");
 
 });
 
-router.post("/", async (req, res) => {
+router.get("/guest", async (req, res) => {
 
-  // new BookingModel({
-  //   count: 4,
-  //   date: new Date(2020, 08, 05),
-  //   time: 18.00,
-  //   guestId: 999,
-  //   id: 3,
-  // }).save();
+  const guest = await GuestModel.find()
+
+  res.send(guest);
+
+});
+
+router.get("/table", async (req, res) => {
+
+  const booking = await BookingModel.find()
+
+  res.send(booking);
+
+});
+
+router.post("/table", async (req, res) => {
+
+  new BookingModel({
+    count: 4,
+    date: new Date(2020, 08, 05),
+    time: 18.00,
+    guestId: 999,
+    id: 3,
+  }).save();
 
 });
 
