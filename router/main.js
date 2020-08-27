141e0aaeb0b1a5ae06fd2b02847a36f730dc6200
Jonathan hannerfeldt <hannerfeldt@hotmail.se>
2020-08-27 15:27:15 +0200

    no overbooking, deleteall, id's added

--- router/main.js
+++ router/main.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const BookingModel = require("../models/booking");
 const GuestModel = require("../models/guest");
+const Booking = require("../models/booking");
 
-router.get("/",(req, res) => {
+router.get("/", (req, res) => {
 
- res.render("index");
+  res.render("index");
 
 });
 
@@ -28,20 +29,90 @@ router.get("/table", async (req, res) => {
 
 router.post("/table", async (req, res) => {
 
+  console.log(req.body.guestId)
+
   new BookingModel({
-    count: 4,
-    date: new Date(2020, 08, 05),
-    time: 18.00,
-    guestId: 999,
-    id: 3,
+    count: req.body.tables.count,
+    date: req.body.tables.date,
+    time: req.body.tables.time,
+    guestId: req.body.guestId,
+    id: 1000,
   }).save();
 
+  res.send({
+    success: true
+  })
 });
+
 
 router.post("/guest", async (req, res) => {
+  let guestId
+  const registered = await GuestModel.findOne({
+    email: req.body.email
+  })
 
-  console.log(req.body);
+  if (registered) {
+    guestId = registered.id
+
+    new GuestModel({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      phonenr: req.body.phonenr,
+      id: registered.id
+    }).save()
+
+  } else {
+    const latest = await GuestModel.findOne().sort({
+      id: -1
+    })
+
+    if (latest) guestId = latest.id + 1
+    else guestId = 1000
+
+    new GuestModel({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      phonenr: req.body.phonenr,
+      id: guestId
+    }).save()
+  }
+
+  res.send({
+    guestId
+  })
 
 });
+
+router.post("/deleteall", async (req, res) => {
+
+  await BookingModel.deleteMany({})
+  await GuestModel.deleteMany({})
+})
+
+router.post("/availablility", async (req, res) => {
+
+  const available = BookingModel.find({
+    date: req.body.date,
+    time: req.body.time
+  }).then(items => {
+    let amount = 0
+    items.forEach(item => {
+      amount += item.count
+    });
+    if (req.body.count + amount > 15) {
+      res.send({
+        success: false
+      })
+    } else {
+      res.send({
+        success: true
+      })
+    }
+  })
+
+  //res.send({available: available})
+})
 
 module.exports = router;
