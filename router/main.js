dea1240f033a9343b7b44cf18303d761df3f8b5a
Julia Nilsson <nilsson.juliaelin@gmail.com>
2020-08-28 13:19:15 +0200

    no longer possible to overbook

--- router/main.js
+++ router/main.js
@@ -4,114 +4,124 @@ const router = express.Router();
 const BookingModel = require("../models/booking");
 const GuestModel = require("../models/guest");
 
-
 router.get("/", (req, res) => {
-
   res.render("index");
-
 });
 
 router.get("/guest", async (req, res) => {
-
-  const guest = await GuestModel.find()
+  const guest = await GuestModel.find();
 
   res.send(guest);
-
 });
 
 router.get("/table", async (req, res) => {
-
-  const booking = await BookingModel.find()
+  const booking = await BookingModel.find();
 
   res.send(booking);
-
 });
 
 router.post("/table", async (req, res) => {
+  const latest = await BookingModel.findOne().sort({
+    id: -1,
+  });
 
-  console.log(req.body.guestId)
+  console.log(latest);
 
   new BookingModel({
     count: req.body.tables.count,
     date: req.body.tables.date,
     time: req.body.tables.time,
     guestId: req.body.guestId,
-    id: 1000,
+    id: latest ? latest.id + 1 : 1000,
   }).save();
 
   res.send({
-    success: true
-  })
+    success: true,
+  });
 });
 
-
 router.post("/guest", async (req, res) => {
-  let guestId
+  let guestId;
   const registered = await GuestModel.findOne({
-    email: req.body.email
-  })
+    email: req.body.email,
+  });
 
   if (registered) {
-    guestId = registered.id
+    guestId = registered.id;
 
     new GuestModel({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       email: req.body.email,
       phonenr: req.body.phonenr,
-      id: registered.id
-    }).save()
-
+      id: registered.id,
+    }).save();
   } else {
     const latest = await GuestModel.findOne().sort({
-      id: -1
-    })
+      id: -1,
+    });
 
-    if (latest) guestId = latest.id + 1
-    else guestId = 1000
+    if (latest) guestId = latest.id + 1;
+    else guestId = 1000;
 
     new GuestModel({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       email: req.body.email,
       phonenr: req.body.phonenr,
-      id: guestId
-    }).save()
+      id: guestId,
+    }).save();
   }
 
   res.send({
-    guestId
-  })
-
+    guestId,
+  });
 });
 
 router.post("/deleteall", async (req, res) => {
-  await BookingModel.deleteMany({})
-  await GuestModel.deleteMany({})
-})
+  await BookingModel.deleteMany({});
+  await GuestModel.deleteMany({});
+});
 
-router.post("/availablility", async (req, res) => {
+router.post("/availability", async (req, res) => {
 
   BookingModel.find({
     date: req.body.date,
-    time: req.body.time
-  }).then(bookingsFound => {
-    let extra = 0 
-    bookingsFound.forEach(booking => { 
-      if(booking.count>6) extra++
-    })
-    if (bookingsFound.length+extra >= 15) {
+    time: req.body.time,
+  }).then((bookingsFound)=> {
+    let extra = 0;
+    let othersuccess = false;
+    bookingsFound.forEach((booking) => {
+      if (booking.count > 6) extra++;
+    });
+    if (bookingsFound.length + extra >= 15) {
+      BookingModel.find({
+        date: req.body.date,
+        time: req.body.time == 21 ? 18 : 21,
+      }).then((othertime) => {
+        let extra = 0;
+        
+        othertime.forEach((booking) => {
+          if (booking.count > 6) extra++;
+        });
+        if (othertime.length + extra >= 15) {
+          othersuccess = false;
+        } else {
+          othersuccess = true;
+        }
+      });
       res.send({
-        success: false
-      })
+        success: false,
+        othersuccess: othersuccess,
+      });
     } else {
       res.send({
-        success: true
-      })
+        success: true,
+        othersuccess: othersuccess,
+      });
     }
-  })
-
+  });
   //res.send({available: available})
-})
+});
 
 module.exports = router;
